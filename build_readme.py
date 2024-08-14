7dcfeb325baf70027315fd527d8048b2dfaea194
Kirti S <shilwantkirti@gmail.com>
2024-08-14 14:51:00 +0530

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -1,143 +1,73 @@
-from python_graphql_client import GraphqlClient
 import feedparser
 import httpx
-import json
 import pathlib
 import re
 import os
+import requests
+import git
 
 root = pathlib.Path(__file__).parent.resolve()
-client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
-
-TOKEN = os.environ.get("SIMONW_TOKEN", "")
-
-
-def replace_chunk(content, marker, chunk):
+def replace_chunk(content, marker, chunk, inline=False):
     r = re.compile(
         r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
         re.DOTALL,
     )
-    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
     return r.sub(chunk, content)
 
+def get_tils():
+    til_readme = "https://raw.githubusercontent.com/shilwantk/til/main/README.md"
+    r = requests.get(til_readme)
 
-def make_query(after_cursor=None):
-    return """
-query {
-  viewer {
-    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
-      nodes {
-        name
-        releases(last:1) {
-          totalCount
-          nodes {
-            name
-            publishedAt
-            url
-          }
-        }
-      }
-    }
-  }
-}
-""".replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
+    page = requests.get(til_readme)
+    all_text = page.text
+    search_re = re.findall( r'(\*+).(\[.*?\])(\(.*?\)).?-(.+)', all_text, re.M|re.I)
+    dt_til = sorted(search_re, key=lambda search_re: search_re[3], reverse=True)[:3]
+    
+    print('~' * 50)
+    print('dt_til upto 3', dt_til)
+    print('~' * 50)
+        
+    til_md = ""
+    
+    for i in dt_til:
+        til_md += "\n" + i[0] + ' ' + i[1] + i[2]         
 
-
-def fetch_releases(oauth_token):
-    repos = []
-    releases = []
-    repo_names = set()
-    has_next_page = True
-    after_cursor = None
-
-    while has_next_page:
-        data = client.execute(
-            query=make_query(after_cursor),
-            headers={"Authorization": "Bearer {}".format(oauth_token)},
-        )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-            "hasNextPage"
-        ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-    return releases
-
-
-def fetch_tils():
-    sql = "select title, url, created_utc from til order by created_utc desc limit 5"
-    return httpx.get(
-        "https://til.simonwillison.net/til.json",
-        params={"sql": sql, "_shape": "array",},
-    ).json()
-
+    print('~' * 50)
+    print('til_md upto 3', til_md)
+    print('~' * 50)
+        
+    return til_md
 
 def fetch_blog_entries():
-    entries = feedparser.parse("https://simonwillison.net/atom/entries/")["entries"]
-    return [
-        {
-            "title": entry["title"],
-            "url": entry["link"].split("#")[0],
-            "published": entry["published"].split("T")[0],
-        }
-        for entry in entries
-    ]
 
 
 if __name__ == "__main__":
+
     readme = root / "README.md"
-    releases = fetch_releases(TOKEN)
-    releases.sort(key=lambda r: r["published_at"], reverse=True)
-    md = "\n".join(
-        [
-            "* [{repo} {release}]({url}) - {published_at}".format(**release)
-            for release in releases[:5]
-        ]
-    )
     readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "recent_releases", md)
-
-    tils = fetch_tils()
-    tils_md = "\n".join(
-        [
-            "* [{title}]({url}) - {created_at}".format(
-                title=til["title"],
-                url=til["url"],
-                created_at=til["created_utc"].split("T")[0],
-            )
-            for til in tils
-        ]
-    )
-    rewritten = replace_chunk(rewritten, "tils", tils_md)
-
-    entries = fetch_blog_entries()[:5]
+    
+    entries = fetch_blog_entries()[:3]
     entries_md = "\n".join(
-        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+        # ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+        ["* [{title}]({url})".format(**entry) for entry in entries]
     )
-    rewritten = replace_chunk(rewritten, "blog", entries_md)
+    rewritten = replace_chunk(readme_contents, "blog", entries_md)
 
+    til_readme_contents = get_tils()
+    
+    print('~' * 50)
+    print('til_readme_contents', til_readme_contents)
+    print('~' * 50)
+    
+    rewritten = replace_chunk(rewritten, "tilentries", til_readme_contents)    
+
+    print('~' * 50)
+    print('rewritten after getting tils', rewritten)
+    print('~' * 50)
+    
+    
     readme.open("w").write(rewritten)
